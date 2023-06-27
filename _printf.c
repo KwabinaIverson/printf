4a22e6d856118ae2d5d17622f893b572cf2a86c3
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-06-27 16:51:48 -0700

    Betty fix

--- _printf.c
+++ _printf.c
@@ -5,18 +5,15 @@
  * @format: the unknown variable
  * Return: the number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list var;
 	int count = 0, i = 0;
 	int (*fn)(va_list);
 
-
 	va_start(var, format);
 	if (format == NULL)
 		return (-1);
-
 	while (format[i] != '\0')
 	{
 		if (format[i] != '%')
@@ -32,9 +29,7 @@ int _printf(const char *format, ...)
 				count += write(STDOUT_FILENO, "%%", 1);
 				continue;
 			}
-
 			fn = get_spe(&format[i + 1]);
-
 			if (fn != NULL)
 			{
 				count += fn(var);
